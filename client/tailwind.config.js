4707fcae3fb527fdd1cc627344f7232266cb7283
kammounmedaziz <kammoun.mohamedaziz@esprit.tn>
2025-08-27 19:42:26 +0200

    server

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+import flowbite from 'flowbite/plugin';
+import tailwindScrollbar from 'tailwind-scrollbar';
+
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbite, 
+    tailwindScrollbar
+  ],
+};
