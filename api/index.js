629702608323b73a5a139578d026ea680b734c81
kammounmedaziz <kammoun.mohamedaziz@esprit.tn>
2025-08-28 09:33:06 +0200

    commit

--- api/index.js
+++ api/index.js
@@ -1,20 +1,11 @@
-import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
+
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('MongoDb is connected');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-const app = express();
-
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB is connected"))
+  .catch(err => console.error("❌ Mongo connection error:", err));
